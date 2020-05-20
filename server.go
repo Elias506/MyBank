09dde759d2ef2d4609c19159f193fb72a7b1ed8f
Elias506 <56119080+Elias506@users.noreply.github.com>
2020-05-20 14:54:11 +0300

    Update server.go

--- server.go
+++ server.go
@@ -11,8 +11,8 @@ import (
 )
 
 type user struct {
-	ID 	int `json:"id,int,omitempty"`
-	Balance int `json:"balance,int"`
+	ID 		int	     `json:"id,int,omitempty"`
+	Balance int		 `json:"balance,int"`
 }
 
 type transfer struct {
@@ -22,6 +22,11 @@ type transfer struct {
 	Amount int `json:"amount,int"`
 }
 
+func handleError(w http.ResponseWriter, url string, err error, status int){
+	fmt.Printf("resource %s err: %s\n", url, err)
+	http.Error(w, "remote resource error", status)
+}
+
 func main() {
 	/*connStr := "user=postgres password=mypass dbname=productdb sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
@@ -29,25 +34,23 @@ func main() {
 		log.Fatal("DataBased error: ", err)
 	}
 	defer db.Close()*/
-
 	http.HandleFunc("/balance",  balanceHandler)
 	http.HandleFunc("/deposit",  depositHandler)
 	http.HandleFunc("/withdraw", withdrawHandler)
 	http.HandleFunc("/transfer", transferHandler)
-
+	
 	fmt.Println("Server is listening...")
-	err = http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe error: ", err)
 	}
-
 }
 
 func balanceHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("user")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintf(w, "User Id is %d\n", id)
@@ -58,35 +61,34 @@ func balanceHandler(w http.ResponseWriter, r *http.Request) {
 
 	//Инициализируем баланс
 	balance := 21
-
 	//Подготовка к выводу
+	
 	u := &user{Balance: balance}
 	uJson, err := json.Marshal(u)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
 		return
 	}
-
+	
 	fmt.Fprintln(w, http.StatusOK, "OK")
 	fmt.Fprintf(w, "%s\n", string(uJson))
 }
-
 
 func depositHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 	t := &transfer{}
 	err = json.Unmarshal(body, t)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
 		return
 	}
 	if t.User == 0 {
-		http.Error(w, "Wrong User Id", http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 
@@ -99,18 +101,18 @@ func depositHandler(w http.ResponseWriter, r *http.Request) {
 func withdrawHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
 	t := &transfer{}
 	err = json.Unmarshal(body, t)
 	if err != nil {
-		fmt.Fprintf(w, "Error: %v", err.Error())
+		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
 		return
 	}
 	if t.User == 0 {
-		http.Error(w, "Wrong User Id", http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 
@@ -124,24 +126,23 @@ func transferHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "transferHandler")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
 	t := &transfer{}
 	err = json.Unmarshal(body, t)
 	if err != nil {
-		http.Error(w, "Wrong User Id", http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
 		return
 	}
 	if t.From == 0 || t.To == 0 {
-		http.Error(w, "Wrong User Id", http.StatusUnprocessableEntity)
+		handleError(w, r.URL.String(), err, http.StatusBadRequest)
 		return
 	}
 
 	//Манипуляции с переводом
-
-	//Вывод
+	//Затем вывод
 
 	fmt.Fprintln(w, http.StatusOK, "OK")
 }
