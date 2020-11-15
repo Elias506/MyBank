df02596fab2e81adc744d81bdf72ed873f23f558
Elias506 <56119080+Elias506@users.noreply.github.com>
2020-11-15 22:56:04 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,142 +1,20 @@
 package main
 
 import (
-	"encoding/json"
+	"./handlers"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"strconv"
 )
 
-type user struct {
-	ID      int `json:"id,int,omitempty"`
-	Balance int `json:"balance,int"`
-}
-
-type transfer struct {
-	User   int `json:"user,int,omitempty"`
-	From   int `json:"from,int,omitempty"`
-	To     int `json:"to,int,omitempty"`
-	Amount int `json:"amount,int"`
-}
-
-func handleError(w http.ResponseWriter, url string, err error, status int){
-	fmt.Printf("resource %s err: %s\n", url, err)
-	http.Error(w, "remote resource error", status)
-}
-
 func main() {
-	http.HandleFunc("/balance",  balanceHandler)
-	http.HandleFunc("/deposit",  depositHandler)
-	http.HandleFunc("/withdraw", withdrawHandler)
-	http.HandleFunc("/transfer", transferHandler)
+	http.HandleFunc("/balance",  handlers.BalanceHandler)
+	http.HandleFunc("/deposit",  handlers.DepositHandler)
+	http.HandleFunc("/withdraw", handlers.WithdrawHandler)
+	http.HandleFunc("/transfer", handlers.TransferHandler)
 
-	fmt.Println("Server is listening...")
-
+	fmt.Println(">Server is listening...")
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe error: ", err)
+		fmt.Println("ListenAndServe error: ", err)
 	}
-}
-
-func balanceHandler(w http.ResponseWriter, r *http.Request) {
-	idStr := r.URL.Query().Get("user")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(w, "User Id is %d\n", id)
-
-	//Поиск id в БД
-	fmt.Fprintln(w, "Search in DataBase...")
-	//Если такого нет, создаем новый
-
-	//Инициализируем баланс
-	balance := 21
-	//Подготовка к выводу
-
-	u := &user{Balance: balance}
-	uJSON, err := json.Marshal(u)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintln(w, http.StatusOK, "OK")
-	fmt.Fprintf(w, "%s\n", string(uJSON))
-}
-
-func depositHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-	t := &transfer{}
-	err = json.Unmarshal(body, t)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
-		return
-	}
-	if t.User == 0 {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintln(w, t.User, t.Amount)
-	//Работа с базой данных
-
-	fmt.Fprintln(w, http.StatusOK, "OK")
-}
-
-func withdrawHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-	t := &transfer{}
-	err = json.Unmarshal(body, t)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
-		return
-	}
-	if t.User == 0 {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintln(w, t.User, t.Amount)
-	//Работа с базой данных
-
-	fmt.Fprintln(w, http.StatusOK, "OK")
-}
-
-func transferHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "transferHandler")
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-	t := &transfer{}
-	err = json.Unmarshal(body, t)
-	if err != nil {
-		handleError(w, r.URL.String(), err, http.StatusInternalServerError)
-		return
-	}
-	if t.From == 0 || t.To == 0 {
-		handleError(w, r.URL.String(), err, http.StatusBadRequest)
-		return
-	}
-
-	//Манипуляции с переводом
-	//Затем вывод
-
-	fmt.Fprintln(w, http.StatusOK, "OK")
 }
